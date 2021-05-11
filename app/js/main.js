d673a52d002861b0bb72d26f508b7f9955d2e9e0
UncleScrooge2012 <scrooge2024@yandex.ru>
2021-05-11 20:46:54 +0300

    feat section hero

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,11 @@
 const menuBurger = document.querySelector('.menu__icon');
 const openMenu = document.querySelector('.menu__body');
 
+
+
 menuBurger.addEventListener('click', e => {
+  menuBurger.classList.toggle('active');
   openMenu.classList.toggle('active');
+  document.body.classList.toggle('_lock');
   
 });
